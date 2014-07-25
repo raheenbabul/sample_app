ae6d72027b3b85c81d9efffa730f86c81e4e0253
Raheen Babul <raheen.babul@annet.com>
2014-07-25 14:43:44 +0530

    Finish user edit, update, index, amd destroy actions

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,10 @@ class SessionsController < ApplicationController
 
   def new
   end
+  
+  def index
+ @users = User.all
+  end
 
   def create
   	#render 'new'
@@ -9,7 +13,7 @@ class SessionsController < ApplicationController
   if user && user.authenticate(params[:session][:password])
     # Sign the user in and redirect to the user's show page.
     sign_in user
-      redirect_to user
+      redirect_back_or user
   else
     # Create an error message and re-render the signin form.
     flash.now[:error] = 'Invalid email/password combination' # Not quite right!
