ea07dfe9b53a92b79f9b329172543fff5dcf149a
Raheen Babul <raheen.babul@annet.com>
2014-08-04 19:10:01 +0530

    Add user micropost

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,13 @@ def signed_in?
   def current_user?(user)
     user == current_user
   end
-  
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
   def current_user
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
@@ -29,7 +35,7 @@ def signed_in?
     cookies.delete(:remember_token)
     self.current_user = nil
   end
-  
+
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
     session.delete(:return_to)
@@ -38,5 +44,5 @@ def signed_in?
   def store_location
     session[:return_to] = request.url if request.get?
   end
-  
+
 end
